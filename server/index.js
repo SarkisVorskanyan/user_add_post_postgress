d372b985db216c332343a81c4dd57ef17f242092
Your Name <you@example.com>
2022-03-31 23:12:25 +0400

    eesd

--- server/index.js
+++ server/index.js
@@ -2,14 +2,13 @@ import express from 'express'
 import postRouter from './routes/post_routes.js'
 import userRouter from './routes/user_routes.js'
 import path from 'path'
-// import file from '../client/public'
+import cors from 'cors'
 
 const app = express()
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(express.static("../client/public"));
 app.use('/api', userRouter)
 app.use('/api', postRouter)
 
